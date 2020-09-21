b6754217aa628b5a1cf860cd43a92a50daee32b0
YeomJaeSeon <a89541457@gmail.com>
2020-09-21 15:07:28 +0900

    cra안쓰고 react 환경 구축 완료

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <div>cra없이 react환경 구축완성!</div>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    port: 7000,
+  },
+};
